007042d0fb60cded536ff9aad2072475c0fb611b
Oliver Coldwell <olivercoldwell@gmail.com>
2021-05-11 16:48:51 +0100

    solution for finding consecutive numbers in strings with dashes, seperated out classes

---
+++ StringsCsharp/ConsecutiveNumbers/AreConsecutive.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ConsecutiveNumbers
+{
+    class AreConsecutive
+    {
+        public static bool Consecutive(IReadOnlyList<int> numbers)
+        {
+            for (var i = 1; i < numbers.Count; ++i)
+            {
+                if (numbers[i] != numbers[i - 1] + 1) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ StringsCsharp/ConsecutiveNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsecutiveNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a few numbers seperated by a hyphen: ");
+            var input = Console.ReadLine();
+            var inputParsed = input.Split('-').Select(int.Parse).ToList();
+            if (AreConsecutive.Consecutive(inputParsed))
+                Console.WriteLine("Consecutive");
+        }
+    }
+}
