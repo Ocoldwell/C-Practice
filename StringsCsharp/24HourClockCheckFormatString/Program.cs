5f02e433837f139f117715f4e1cc7a91147c42b1
Oliver Coldwell <olivercoldwell@gmail.com>
2021-05-13 12:55:47 +0100

    checks string is in format of 24 hour clock and between set times

---
+++ StringsCsharp/24HourClockCheckFormatString/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _24HourClockCheckFormatString
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a time between 00:00 and 23:59: ");
+            var input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Invalid time");
+            }
+            else
+            {
+                var hour = Convert.ToInt32(input?.Split(':')[0]);
+                var minute = Convert.ToInt32(input?.Split(':')[1]);
+
+                if (isValidHour(hour) && isValidMinute(minute))
+                {
+                    Console.WriteLine("Ok");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Time");
+                }
+            }
+        }
+        private static bool isValidHour(int hour)
+        {
+            return hour >= 0 && hour <= 23;
+        }
+        private static bool isValidMinute(int minute)
+        {
+            return minute >= 0 && minute <= 59;
+        }
+    }
+}
