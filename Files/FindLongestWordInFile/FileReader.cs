f249f5f2d624d750f9ef8dde512bffec470b1224
Oliver Coldwell <olivercoldwell@gmail.com>
2021-05-15 12:48:11 +0100

    finds longest wordsin a file, punctuation breaks it

---
+++ Files/FindLongestWordInFile/FileReader.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Text;
+
+namespace FindLongestWordInFile
+{
+    class FileReader
+    {
+        public static string ReadFileToString(string filePath)
+        {
+            var streamReader = new StreamReader(@filePath, Encoding.UTF8);
+            return streamReader.ReadToEnd();
+        }
+    }
+}
---
+++ Files/FindLongestWordInFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FindLongestWordInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FILE_PATH ="/Users/olliecoldwell/Projects/C#Practice/Files/FindLongestWordInFile/test.txt";
+
+            var text = FileReader.ReadFileToString(FILE_PATH);
+            var words = text.Split(' ');
+            string longestWord = "";
+            foreach (var word in words)
+            {
+                var tmpWord = word;
+                if (tmpWord.Length > longestWord.Length)
+                {
+                    longestWord = tmpWord;
+                }
+            }
+            Console.WriteLine("The longest word is {0} at {1}", longestWord, longestWord.Length);
+        }
+    }
+    
+}
